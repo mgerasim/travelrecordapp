00da4764268177c8f42835b3c77a362c7207f6c1
Mikhail Gerasimov <mgerasim@inbox.ru>
2018-07-27 08:49:33 +1000

    init

---
+++ travelrecordapp/ViewModels/ItemDetailViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+using travelrecordapp.Models;
+
+namespace travelrecordapp.ViewModels
+{
+    public class ItemDetailViewModel : BaseViewModel
+    {
+        public Item Item { get; set; }
+        public ItemDetailViewModel(Item item = null)
+        {
+            Title = item?.Text;
+            Item = item;
+        }
+    }
+}
---
+++ travelrecordapp/Views/MainPage.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace travelrecordapp.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class MainPage : TabbedPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+       
+    }
+}
