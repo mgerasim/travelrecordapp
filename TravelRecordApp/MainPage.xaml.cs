155c1b5366066584958abc6dea6a272bf854e05d
Mikhail Gerasimov <mgerasim@inbox.ru>
2018-07-28 07:46:08 +1000

    #1 Выполнить пункт 26 Create the Login Page #1

---
+++ TravelRecordApp/MainPage.xaml.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace TravelRecordApp
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        void LoginHandle_Clicked(object sender, System.EventArgs e)
+        {
+            
+        }
+    }
+}
