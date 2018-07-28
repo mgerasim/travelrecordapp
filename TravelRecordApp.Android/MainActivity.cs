a82484bc57be66714c58970d2dfc4cbf32b5e701
Mikhail Gerasimov <mgerasim@inbox.ru>
2018-07-28 10:52:05 +1000

    Реализовать п 50-52 подключение SQLite #8

--- TravelRecordApp.Android/MainActivity.cs
+++ TravelRecordApp.Android/MainActivity.cs
@@ -6,6 +6,7 @@ using Android.Runtime;
 using Android.Views;
 using Android.Widget;
 using Android.OS;
+using System.IO;
 
 namespace TravelRecordApp.Droid
 {
@@ -20,7 +21,12 @@ namespace TravelRecordApp.Droid
             base.OnCreate(bundle);
 
             global::Xamarin.Forms.Forms.Init(this, bundle);
-            LoadApplication(new App());
+
+            string dbName = "travel_db.sqlite";
+            string folderPath = System.Environment.GetFolderPath(System.Environment.SpecialFolder.Personal);
+            string fullPath = Path.Combine(folderPath, dbName);
+
+            LoadApplication(new App(fullPath));
         }
     }
 }
--- TravelRecordApp/App.xaml.cs
+++ TravelRecordApp/App.xaml.cs
@@ -7,6 +7,8 @@ namespace TravelRecordApp
 {
     public partial class App : Application
     {
+        public static string DatabaseLocation = string.Empty;
+
         public App()
         {
             InitializeComponent();
@@ -14,6 +16,14 @@ namespace TravelRecordApp
             MainPage = new NavigationPage(new MainPage());
         }
 
+		public App(string databaseLocation)
+        {
+            InitializeComponent();
+
+            MainPage = new NavigationPage(new MainPage());
+
+            DatabaseLocation = databaseLocation;
+        }
         protected override void OnStart()
         {
             // Handle when your app starts
